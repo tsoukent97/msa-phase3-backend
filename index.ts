c0ce2f6f43ec4c28b691b01a76b48db678d5ca66
Kent Tsou <tsoukent97@gmail.com>
2022-09-16 14:11:04 +1200

    removed unwanted data

--- index.ts
+++ index.ts
@@ -9,9 +9,6 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
   type aircraft_data {
     model: String
     range: String
@@ -27,9 +24,6 @@ const typeDefs = gql`
     player_team: String
   }
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
     aircrafts_data: [aircraft_data]
     airplanes_data: [airport_data]
